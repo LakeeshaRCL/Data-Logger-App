d2601d8d0776ffcaffb08c6a6e5dace2164f5dac
Lakeesha Ramanayake <65113676+LakeeshaRCL@users.noreply.github.com>
2021-11-18 10:30:26 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# data_logger_app
+
+A new Flutter project for data logger application
+
---
+++ android/app/src/main/kotlin/com/example/data_logger_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.data_logger_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
